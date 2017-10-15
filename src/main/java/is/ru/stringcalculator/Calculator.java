cb1a79ece863180a833b4aa562a4b66ee537fe75
Andri Sveinbjörnsson <andris16@ru.is>
2017-10-15 02:04:38 +0000

    Testing two numbers

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -3,13 +3,27 @@ package is.ru.stringcalculator;
 public	class	Calculator	{
 
 	public	static int add(String text){
-		if(text.equals(""))
-		{
+		if(text.equals("")){
 			return 0;
 		}
-		else
-		{
+    else if (text.contains(",")) {
+      String numbers[] = text.split(",");
+      return sum(numbers);
+    }
+		else{
 			return Integer.parseInt(text);
 		}
 	}
+
+  private static int toInt(String number){
+  	return Integer.parseInt(number);
+  }
+  private static int sum(String [] numbers){
+  	int total = 0;
+  	for(int i = 0; i < numbers.length; i++){
+  		total += toInt(numbers[i]);
+  	}
+  	return total;
+  }
+
 }
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -13,4 +13,8 @@ public class CalculatorTest{
     public void testOneString() {
         assertEquals(1, Calculator.add("1"));
     }
+  @Test //2
+    public void testTwoString() {
+        assertEquals(3, Calculator.add("1,2"));
+    }
 }
